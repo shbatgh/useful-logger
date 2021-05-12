391d8789408348864c0a6c259c930beb6b527889
shbatgh <shboccara@gmail.com>
2021-05-11 21:35:02 -0400

    Good?

--- test-logio/test.py
+++ test-logio/test.py
@@ -1,10 +1,10 @@
 import random
 import time
 
-log_file = open('/home/sam/Dev/useful-logger/test-logio/var/logs/test.log', 'a')
+log_file = open('/home/sam/Dev/useful-logger/test-logio/var/logs/test.log', 'a', 0)
 
-for i in range(0, 5):
-    log_file.write(f'my random: {random.randint(1, 300)}\n')
+for i in range(0, 100):
+    log_file.write(b'my random: {random.randint(1, 300)}\n')
     time.sleep(1)
 
 # print(random())
